c74612cccc2faa229145779db8a9e7a354256220
7022Andre <andre.mueller@zoho.com>
2017-03-17 11:04:57 -0700

    Assignment 3: Fix indentation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,20 @@
-(function() {
+(function () {
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
                 enabled: true,
                 requiredBase: false
-        });
-        
+            });
+
         $stateProvider
             .state('landing', {
-            url: '/',
-            templateUrl: '/templates/landing.html'
-        })
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
             .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
-        })
-            .state('collection', {
-            url: '/collection',
-            templateUrl: '/templates/collection.html'
-        });
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
     }
 
     angular
