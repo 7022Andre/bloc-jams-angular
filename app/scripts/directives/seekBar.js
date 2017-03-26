4e7385b6902a6d3cbbc31c02af98c9d567043dd4
7022Andre <andre.mueller@zoho.com>
2017-03-26 11:21:34 -0700

    Checkpoint 10 complete

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -24,7 +24,9 @@
             templateUrl: '/templates/directives/seek_bar.html', // Template where directive will be used
             replace: true, // Directive element <seek-bar> will be replaced with HTML template (rather than inserted between tags)
             restrict: 'E', // Treats directive as element
-            scope: { }, // Creates isolated scope exclusively for this directive
+            scope: {
+                onChange: '&'
+            }, // Creates isolated scope exclusively for this directive
             /**
             * @function link
             * @desc Responsible for registering DOM listeners + updating DOM
@@ -39,6 +41,18 @@
                 */
                 var seekBar = $(element);
                 /**
+                * @desc Watches for changes to attribute 'value' (added as attribute to player_bar.html); sets new scope value to attribute 'value'
+                */
+                attributes.$observe('value', function(newValue) {
+                    scope.value = newValue;
+                });
+                /**
+                * @desc Watches for changes to attribute 'max' (added as attribute to player_bar.html); sets new scope value to attribute 'max'
+                */
+                attributes.$observe('max', function(newValue) {
+                    scope.max = newValue;
+                });
+                /**
                 * @function percentString
                 * @desc Calculates percentage of filled portion of seek bar
                 * @returns string with percentage as value
@@ -48,6 +62,15 @@
                     var max = scope.max;
                     var percent = value / max * 100;
                     return percent + '%';
+                };
+                /**
+                * @function notifyOnChange
+                * @desc Updates value of on-change attribute (added as attribute to player_bar.html)
+                */
+                var notifyOnChange = function (newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
                 };
                 /**
                 * @function scope.fillStyle
@@ -67,21 +90,23 @@
                 };
                 /**
                 * @function scope.onClickSeekbar
-                * @desc Updates seek bar value when user clicks on seek bar
+                * @desc Updates seek bar value when user clicks on seek bar; passes updated value to view
                 */
                 scope.onClickSeekBar = function (event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
                 /**
                 * @function scope.trackThumb
-                * @desc Updates seek bar value as user drags seek bar thumb by binding events to mouseup and mousemove
+                * @desc Updates seek bar value as user drags seek bar thumb by binding events to mouseup and mousemove; passes updated value to view
                 */
                 scope.trackThumb = function () {
                     $document.bind('mousemove.thumb', function (event) {
                         var percent = calculatePercent(seekBar, event);
                         scope.$apply(function () {
                             scope.value = percent * scope.max;
+                            notifyOnChange(scope.value);
                         });
                     });
                     $document.bind('mouseup.thumb', function () {
